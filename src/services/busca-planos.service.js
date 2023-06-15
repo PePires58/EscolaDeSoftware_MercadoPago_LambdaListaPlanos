ce6d101d3b5dab8f57955f04dfcdce22a93eb9e5
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-06-15 13:00:03 -0300

    adicionando header

--- src/services/busca-planos.service.js
+++ src/services/busca-planos.service.js
@@ -4,18 +4,23 @@ exports.buscaPlanosAtivos = async function (secret) {
     const urlGetPlanos = `${process.env.BaseUrlMercadoPago}/preapproval_plan/search?status=active`;
 
     return await new Promise((resolve, reject) => {
-        const req = https.get(urlGetPlanos, function (res) {
+        const req = https.get(urlGetPlanos, {
+            headers: {
+                'Authorization': `Bearer ${secret}`
+            }
+        },
+            function (res) {
 
-            let planosRetorno;
-            res.on('data', planos => {
-                planosRetorno += planos;
-            });
-            res.on('end', () => {
-                resolve({
-                    body: JSON.stringify(planosRetorno)
+                let planosRetorno;
+                res.on('data', planos => {
+                    planosRetorno += planos;
+                });
+                res.on('end', () => {
+                    resolve({
+                        body: planosRetorno
+                    });
                 });
             });
-        });
 
         req.on('error', (e) => {
             reject({
